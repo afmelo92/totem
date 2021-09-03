d32d28a3a0630d847b78cd714cd285c6c3ae0fea
Andre <andre.fabian.melo@gmail.com>
2021-09-03 12:44:09 -0300

    Add jest / react testing library

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// import { Container } from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <main>
+      <h1>Totem App</h1>
+    </main>
+  )
+}
+
+export default Main
